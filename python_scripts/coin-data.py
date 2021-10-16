00d37173b71ccaff632bf944f78f9a4b5d167824
Yens <yens.loff@gmail.com>
2021-10-16 02:43:29 +0200

    buy/sell

--- python_scripts/coin-data.py
+++ python_scripts/coin-data.py
@@ -8,7 +8,7 @@ import csv
 ploads = {
     'vs_currency': 'usd',
     'order': 'market_cap_desc',
-    'per_page': 100,
+    'per_page': 200,
     'page': 1
 }
 
@@ -36,3 +36,25 @@ for x in y:
                 x["symbol"],
                 x["id"]
                 ])
+
+# Write <li> list of data
+f = open("test-list.txt", "w")
+
+f.write("<ul>\n")
+
+count = 0
+for x in y:
+    count += 1
+    id = x["id"]
+    f.write("\t<li>"
+            + f"<a href=\"portfolio?addCoin={id}\" class=\"flex p-2 text-black hover:bg-gray-200 cursor-pointer\">"
+            + "<span><img class=\"w-7\" src=\"" + x["image"] + "\" /></span>"
+            # + " "
+            + "<span class=\"pl-3 mx-0 my-auto\">" + x["name"] + "</span>"
+            + "</a>"
+            + "</li>\n"
+            )
+
+f.write("</ul>")
+
+f.close()
